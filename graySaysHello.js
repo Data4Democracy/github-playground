b459b46cb10aff6995ef51a3eacd9b839ede6402
Grayson Venters <graysonventers@gmail.com>
2021-02-25 22:17:15 -0800

    added new function

--- graySaysHello.js
+++ graySaysHello.js
@@ -1,3 +1,7 @@
 function helloWorld () {
     console.log('Ahhhh yes a friendly but lonely javascript file in a world of snakes');
 }
+
+function helloWorldTwo () {
+    console.log('hello test 2');
+}
